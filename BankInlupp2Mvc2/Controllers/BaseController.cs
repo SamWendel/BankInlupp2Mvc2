8a432b85705f01b93594b0dbab9632a5e199ee2b
SamWendel <sam.wendel@hotmail.se>
2021-04-26 19:46:53 +0200

    Add project files.

---
+++ BankInlupp2Mvc2/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BankInlupp2Mvc2.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankInlupp2Mvc2.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly ApplicationDbContext _dbContext;
+
+        public BaseController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
