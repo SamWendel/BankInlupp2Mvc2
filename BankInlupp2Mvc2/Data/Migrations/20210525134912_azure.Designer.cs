58d1a6c0448b1173b760d02ca6ec93c3c3e5261e
SamWendel <sam.wendel@hotmail.se>
2021-05-25 17:20:27 +0200

    Finalfinal

--- BankInlupp2Mvc2/Data/Migrations/20210525134912_azure.Designer.cs
+++ BankInlupp2Mvc2/Data/Migrations/20210525134912_azure.Designer.cs
@@ -4,14 +4,16 @@ using BankInlupp2Mvc2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankInlupp2Mvc2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210525134912_azure")]
+    partial class azure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BankInlupp2Mvc2/Data/Migrations/20210525134912_azure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankInlupp2Mvc2.Data.Migrations
+{
+    public partial class azure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Customers",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Customers",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
